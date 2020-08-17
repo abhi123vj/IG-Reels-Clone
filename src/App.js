139067f596f9f1c3dea0c9cc718c6b5ace499d24
4bh1 <abhiram123vj@gmail.com>
2020-08-18 01:34:25 +0530

    logo added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hey abhi </h1>
+      <div className="app__top">
+
+      </div>
+      <div className="app__videos">
+        <img className="app__logo"
+        src="https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f2b4c9efc3ed7b0659d27ae%2F0x0.jpg"
+        alt=""
+        />
+        <h1>Reels</h1>
+      </div>
     </div>
   );
 }
