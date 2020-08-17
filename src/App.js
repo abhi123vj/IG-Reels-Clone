98377654d4153e8ef9cb3df9a9d2464268ee3e80
4bh1 <abhiram123vj@gmail.com>
2020-08-18 01:58:55 +0530

    container added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <h1>Hey abhi </h1>
       <div className="app__top">
-
-      </div>
-      <div className="app__videos">
         <img className="app__logo"
-        src="https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f2b4c9efc3ed7b0659d27ae%2F0x0.jpg"
-        alt=""
+          src="https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5f2b4c9efc3ed7b0659d27ae%2F0x0.jpg"
+          alt=""
         />
         <h1>Reels</h1>
+      </div>
+      <div className="app__videos">
+
       </div>
     </div>
   );
